98b92b8e2707c1185cf81cc705894a9d3f104c1a
Edward Wheeler <30383573+sizzeR1337@users.noreply.github.com>
2021-09-26 19:47:28 +0200

    Create glu32.h

---
+++ glu32.h
@@ -0,0 +1,263 @@
+#pragma once
+
+#define EXTERN_DLL_EXPORT extern "C" __declspec(dllexport)
+
+EXTERN_DLL_EXPORT PVOID gluBeginCurve() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluBeginPolygon() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluBeginSurface() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluBeginTrim() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluBuild1DMipmaps() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluBuild2DMipmaps() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluCylinder() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluDeleteNurbsRenderer() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluDeleteQuadric() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluDeleteTess() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluDisk() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluEndCurve() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluEndPolygon() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluEndSurface() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluEndTrim() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluErrorString() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluErrorUnicodeStringEXT() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluGetNurbsProperty() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluGetString() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluGetTessProperty() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluLoadSamplingMatrices() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluLookAt() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluNewNurbsRenderer() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluNewQuadric() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluNewTess() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluNextContour() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluNurbsCallback() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluNurbsCurve() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluNurbsProperty() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluNurbsSurface() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluOrtho2D() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluPartialDisk() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluPerspective() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluPickMatrix() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluProject() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluPwlCurve() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluQuadricCallback() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluQuadricDrawStyle() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluQuadricNormals() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluQuadricOrientation() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluQuadricTexture() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluScaleImage() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluSphere() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluTessBeginContour() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluTessBeginPolygon() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluTessCallback() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluTessEndContour() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluTessEndPolygon() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluTessNormal() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluTessProperty() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluTessVertex() {
+
+    return 0;
+}
+
+EXTERN_DLL_EXPORT PVOID gluUnProject() {
+
+    return 0;
+}
